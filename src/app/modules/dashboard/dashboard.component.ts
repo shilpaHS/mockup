32079c90a112db897323ee9b950252cd0a18904d
Shilpa HS <shilpahalebidu@gmail.com>
2022-01-11 23:12:11 +0530

    Dashboard commit

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DashboardService } from './dashboard.service';
+import { Photo } from './../photo/photo.model'
+import { Post } from './../post/post.model'
+import { Album } from './../album/album.model'
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  albums: Album[] = [];
+  photos: Photo[] = [];
+  posts: Post[] = [];
 
-  constructor() { }
+  constructor(private dashboard_service: DashboardService) { }
 
   ngOnInit(): void {
+    this.fetchAlbum()
+    this.fetchPhoto()
+    this.fetchPosts()
+  }
+  fetchAlbum() {
+    this.dashboard_service.getAlbum().subscribe((data: Album[]) => {
+      this.albums = data
+    },
+      (error) => {
+        console.log("Error accessing Album", error)
+      })
+  }
+  fetchPhoto() {
+    this.dashboard_service.getPhotos().subscribe((data: Photo[]) => {
+      this.photos = data
+    },
+      (error) => {
+        console.log("Error accessing Album", error)
+      })
+  } fetchPosts() {
+    this.dashboard_service.getPosts().subscribe((data: Post[]) => {
+      this.posts = data
+    },
+      (error) => {
+        console.log("Error accessing Album", error)
+      })
   }
 
 }
--- src/app/modules/dashboard/dashboard.service.ts
+++ src/app/modules/dashboard/dashboard.service.ts
@@ -1,13 +1,40 @@
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
-
+import { HttpClient } from '@angular/common/http';
+import { map } from "rxjs/operators";
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { Photo } from './../photo/photo.model'
+import { Post } from './../post/post.model'
+import { Album } from './../album/album.model'
 @Injectable({
     providedIn: 'root'
 })
 export class DashboardService {
-
-    constructor() {
+    private api_url: string = 'https://jsonplaceholder.typicode.com/';
+    constructor(private httpClient: HttpClient) {
+    }
+    getPhotos(): Observable<Photo[]> {
+        return this.httpClient.get(this.api_url + 'photos/')
+            .pipe(map((resp: any) => resp),
+                catchError(error => this.throwError(error))
+            )
+    }
+    getAlbum(): Observable<Album[]> {
+        return this.httpClient.get(this.api_url + 'albums/')
+            .pipe(map((resp: any) => resp),
+                catchError(error => this.throwError(error))
+            )
     }
 
+    getPosts(): Observable<Post[]> {
+        return this.httpClient.get(this.api_url + 'posts/')
+            .pipe(map((resp: any) => resp),
+                catchError(error => this.throwError(error))
+            )
+    }
+    throwError(error: any) {
+        console.error(error);
+        return throwError(error.json().error || 'Server error');
+    }
 }
